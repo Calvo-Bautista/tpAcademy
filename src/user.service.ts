aaf554bab466903fe155f0cb63443152f1a13e1d
Bautista Calvo <calvobautista@gmail.com>
2026-01-18 04:08:31 -0300

    feat(users): add endpoint GET users

--- src/user.service.ts
+++ src/user.service.ts
@@ -1,5 +1,15 @@
-// export class UserService {
-//   getUsers() {
-//     return [];
-//   }
-// }
+
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+    private readonly users = [
+        { id: 1, name: 'Juan', email: 'alice@gmail.com' },
+        { id: 2, name: 'Bautista', email: 'Bautista@gmail.com' },
+        { id: 3, name: 'Hector', email: 'Hector@gmail.com' },
+    ];
+
+    getUsers() {
+        return this.users;
+    }
+}
